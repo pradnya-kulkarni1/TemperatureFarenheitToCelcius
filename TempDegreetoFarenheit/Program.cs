02705d28a3717dbdfbe194bd3443feb709656a79
Pradnya Kulkarni <hellopradnya0509@gmail.com>
2024-02-23 19:46:32 -0500

    Add project files.

---
+++ TempDegreetoFarenheit/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace TempDegreetoFarenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Temperature Converter");
+
+            string UserChoice = "";
+            String YorN = "y";
+            YorN = YorN.ToUpper();
+
+            while (YorN.Equals("Y"))
+            {
+
+                Double Farenheit;
+                Double Celcius;
+                Console.WriteLine("Enter degrees in Farenheit");
+
+                String UserFarenheit = Console.ReadLine();
+                Farenheit = Double.Parse(UserFarenheit);
+
+                //Console.WriteLine("Your number is "+Farenheit);
+
+                Celcius = (Farenheit - 32) * 5 / 9;
+                Celcius = Math.Round(Celcius, 2);
+                Console.WriteLine($"Degrees in Celcius : {Celcius}");
+                Boolean Success = false;
+
+                while (!Success) { 
+                Console.WriteLine("Continue Y/N?");
+                UserChoice = Console.ReadLine();
+                UserChoice = UserChoice.ToUpper();
+
+                    if (UserChoice == "Y")
+                    {
+                        YorN = UserChoice;
+                        Success = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (UserChoice == "N")
+                        {
+                            YorN = UserChoice;
+                            Success = true;
+                            break;
+                        }
+                        else
+                            Console.WriteLine("Please enter Y or N");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
